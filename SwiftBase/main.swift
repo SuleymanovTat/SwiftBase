125a36e1f156d497eb3baa8e6834b85cbc193a03
i.suleymanov <i.suleymanov@iportal.ru>
2022-03-15 21:10:58 +0500

    swift base

--- SwiftBase/main.swift
+++ SwiftBase/main.swift
@@ -4,8 +4,48 @@
 //
 //  Created by Ильмир Сулейманов on 15.03.2022.
 //
-
 import Foundation
 
-print("Hello, World!")
+//let константы
+//var переменны
+var a = 1
+var b = 2
+let name = 102
+var c = a + b + name
+let userName = "Ivan"
+print("Hello, World! ", c)
+newState()
+print(newState2())
+
+//func
+func newState(){
+    print("newState")
+}
+//func return
+func newState2() -> Int{
+  return 10
+}
+//типы данных int разные Int
+let numberA: Int = 1
+let numberAA: UInt = 1
+let numberAAA: UInt64 = 1
+let isNumber: Bool = true
+let numberC: Double = 5.5
+let numberF: Float = 5.5
+let numberF64: Float64 = 5.5
+let str: String = "100"
+print(str)
+print("number \(numberA)")
+
+//условия
+if isNumber {
+    print("true")
+} else {
+    print("false")
+}
+//примедения типов
+let aa = a + Int(numberF)
+print("aa ", aa)
+
+
 
